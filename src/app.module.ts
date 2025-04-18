5243fa53b06d0a8099008ad48351608abb2f6617
arlansantos <arlan05sts@gmail.com>
2025-04-17 21:28:06 -0300

    feat: authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthGuard } from './auth/guard/auth.guard';
+import { RolesGuard } from './auth/guard/roles.guard';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: RolesGuard
+    }
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,20 +2,30 @@ import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { AuthGuard } from './guard/auth.guard';
 import { LoginDto } from './dto/login.dto';
+import { RolesGuard } from './guard/roles.guard';
+import { Roles } from './decorator/roles.decorator';
+import { Role } from './enum/role.enum';
+import { Public } from './decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async signIn(@Body() loginDto: LoginDto){
     return this.authService.signIn(loginDto.username, loginDto.password)
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
   }
-  
+
+  @Roles(Role.Admin)
+  @Get('profile/admin')
+  getProfileAdmin(@Request() req) {
+    return req.user;
+  }
+
 }
